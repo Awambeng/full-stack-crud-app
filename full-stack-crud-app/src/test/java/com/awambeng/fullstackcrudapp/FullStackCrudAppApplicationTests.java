e701fac86f13c5e888c171dc18ca558d07a779cf
Awambeng <awambengrodrick@gmail.com>
2024-03-18 08:44:01 +0100

    add project setup

---
+++ full-stack-crud-app/src/test/java/com/awambeng/fullstackcrudapp/FullStackCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awambeng.fullstackcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
